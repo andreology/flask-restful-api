893ab96b5f4a059b9e59a51cb3179c3788f55267
andreology <barajas.andre@gmail.com>
2022-11-25 18:20:35 -0800

    adding authentication

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
+from crypt import methods
 from re import S
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String, Float
 from flask_marshmallow import Marshmallow
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token
 
 import os
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'planets.db')
+app.config['JWT_SECRET_KEY'] = 'super-secret'
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+jwt = JWTManager(app)
 
 @app.cli.command('db_create')
 def db_create(): 
@@ -76,6 +80,39 @@ def planets():
     result = planets_schema.dump(planets_list)
     print(result)
     return jsonify(result)
+
+@app.route('/register', methods=['POST'])
+def register():
+    email = request.form['email']
+    test = User.query.filter_by(email=email).first()
+    if test:
+        return jsonify(message='Email already exists'), 409
+    else:
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        password = request.form['password']
+        user = User(first_name = first_name, last_name=last_name, email= email, password=password)
+        db.session.add(user)
+        db.session.commit()
+        return jsonify(message='User created'), 201
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    if request.is_json:
+        email = request.json['email']
+        password = request.json['password']
+    else:
+        email = request.form['email']
+        password = request.form['password']
+    
+    test = User.query.filter_by(email=email, password=password).first()
+    if test:
+        access_token = create_access_token(identity=email)
+        return jsonify(message='Login Succeeded', access_token=access_token)
+    else:
+        return jsonify(message='Bad email or password'), 401
+
 
 #database models
 class User(db.Model):
